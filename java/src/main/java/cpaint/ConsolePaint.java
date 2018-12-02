99b5967e2fc3dd9e84cfe159e57a4d027e25dd90
Gabriele Cipriano <gabri.cipriano@gmail.com>
2018-12-02 11:21:55 +0000

    multiple inputs are testable

--- java/src/main/java/cpaint/ConsolePaint.java
+++ java/src/main/java/cpaint/ConsolePaint.java
@@ -2,14 +2,20 @@ package cpaint;
 
 import java.util.Scanner;
 
+import static java.lang.String.format;
+
 public class ConsolePaint {
     public static void main(String[] args) {
         new ConsolePaint().executeWith(null);
     }
 
     public void executeWith(Scanner inputSource) {
+        System.out.println("What's your name?: ");
         String name = inputSource.nextLine();
-        System.out.printf("Hello %s!", name);
+        System.out.println(format("Hello %s!", name));
+        System.out.println("What's your name again?: ");
+        String repeatedName = inputSource.nextLine();
+        System.out.print(format("Oh ok %s, nice to meet you", repeatedName));
     }
 
 }
--- java/src/test/java/cpaint/AcceptanceTest.java
+++ java/src/test/java/cpaint/AcceptanceTest.java
@@ -1,12 +1,11 @@
 package cpaint;
 
+import org.apache.tools.ant.filters.StringInputStream;
 import org.junit.jupiter.api.Test;
 
-import org.apache.tools.ant.filters.StringInputStream;
 import java.io.ByteArrayOutputStream;
 import java.io.PrintStream;
 import java.util.Scanner;
-import java.util.function.Consumer;
 
 import static org.hamcrest.CoreMatchers.is;
 import static org.hamcrest.MatcherAssert.assertThat;
@@ -14,25 +13,23 @@ import static org.hamcrest.MatcherAssert.assertThat;
 class AcceptanceTest {
     @Test
     void spike_system_out() throws Exception {
-        executeTrappingConsoleOutput(
-                consoleOutput -> {
-
-                    new ConsolePaint()
-                            .executeWith(new Scanner(new StringInputStream("Gabriele")));
-
-                    assertThat(consoleOutput.toString(), is("Hello Gabriele!"));
-                });
-    }
-
-    private void executeTrappingConsoleOutput(Consumer<ByteArrayOutputStream> test) throws Exception {
         PrintStream originalOut = System.out;
 
         try (ByteArrayOutputStream consoleOutput = new ByteArrayOutputStream();
-             PrintStream printStream = new PrintStream(consoleOutput))
-        {
+             PrintStream printStream = new PrintStream(consoleOutput)) {
             System.setOut(printStream);
 
-            test.accept(consoleOutput);
+            new ConsolePaint()
+                    .executeWith(new Scanner(
+                            new StringInputStream(
+                                    "Gabriele" + System.lineSeparator() +
+                                            "Giovanni")));
+
+            assertThat(consoleOutput.toString(), is(
+                    "What's your name?: " + System.lineSeparator() +
+                    "Hello Gabriele!" + System.lineSeparator() +
+                    "What's your name again?: " + System.lineSeparator() +
+                    "Oh ok Giovanni, nice to meet you"));
 
         } finally {
             System.setOut(originalOut);
