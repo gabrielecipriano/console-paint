dc5cebd1ef9817b260bc30573dadf21552adf80f
Gabriele Cipriano <gabri.cipriano@gmail.com>
2018-12-03 22:33:41 +0000

    scenarios in one test

--- java/src/test/java/cpaint/GivenScenarios.java
+++ java/src/test/java/cpaint/GivenScenarios.java
@@ -6,58 +6,11 @@ import static cpaint.DSLAcceptanceTests.*;
 
 class GivenScenarios {
     @Test
-    void draw_a_canvas() throws Exception {
-        check(withInput("C 20 5"),
-
-                outputIs("enter command: ",
-                        "----------------------",
-                        "|                    |",
-                        "|                    |",
-                        "|                    |",
-                        "|                    |",
-                        "|                    |",
-                        "----------------------",
-                        "enter command: "));
-    }
-
-    @Test
-    void draw_a_line_within_a_canvas() throws Exception {
+    void draw_a_canvas_a_line_a_rectangle() throws Exception {
         check(withInput(
                 "C 20 5",
                 "L 1 3 7 3",
-                "L 7 1 7 3"),
-
-                outputIs("enter command: ",
-                        "----------------------",
-                        "|                    |",
-                        "|                    |",
-                        "|                    |",
-                        "|                    |",
-                        "|                    |",
-                        "----------------------",
-                        "enter command: ",
-                        "----------------------",
-                        "|                    |",
-                        "|                    |",
-                        "|xxxxxxx             |",
-                        "|                    |",
-                        "|                    |",
-                        "----------------------",
-                        "enter command: ",
-                        "----------------------",
-                        "|      x             |",
-                        "|      x             |",
-                        "|xxxxxxx             |",
-                        "|                    |",
-                        "|                    |",
-                        "----------------------",
-                        "enter command: "));
-    }
-
-    @Test
-    void draw_a_rectangle_within_a_canvas() throws Exception {
-        check(withInput(
-                "C 20 5",
+                "L 7 1 7 3",
                 "R 15 2 20 5"),
 
                 outputIs("enter command: ",
@@ -65,6 +18,22 @@ class GivenScenarios {
                         "|                    |",
                         "|                    |",
                         "|                    |",
+                        "|                    |",
+                        "|                    |",
+                        "----------------------",
+                        "enter command: ",
+                        "----------------------",
+                        "|                    |",
+                        "|                    |",
+                        "|xxxxxxx             |",
+                        "|                    |",
+                        "|                    |",
+                        "----------------------",
+                        "enter command: ",
+                        "----------------------",
+                        "|      x             |",
+                        "|      x             |",
+                        "|xxxxxxx             |",
                         "|                    |",
                         "|                    |",
                         "----------------------",
